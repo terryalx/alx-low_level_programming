a2d730461a361b7df48fc4c5c41f86e668654dd9
setalxswe <setalxswe@gmail.com>
2023-06-20 13:02:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet -> prints the lowercase alphabets
+*/
+
+void print_alphabet(void)
+{
+	int alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
