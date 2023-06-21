9606bc595d3bb78df75cdfe9f510740d34f12f8c
setalxswe <setalxswe@gmail.com>
2023-06-20 22:34:02 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,45 @@
 #include "main.h"
 
 /**
- * print_times_table - time table
+ * print_times_table -> time table
  * @n: number of the times table
  */
 
 void print_times_table(int n)
 {
-	int a;
-    int b;
-    int d;
+	int i, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a <= n; a++)
+		for (i = 0; i <= n; i++)
 		{
-			for (b = 0; b <= n; b++)
+			for (j = 0; j <= n; j++)
 			{
-				d = b * a;
-				if (b == 0)
+				k = j * i;
+				if (j == 0)
 				{
-					_putchar(d + '0');
-				} else if (d < 10 && b != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(d + '0');
-				} else if (d >= 10 && d < 100)
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((d / 10) + '0');
-					_putchar((d % 10) + '0');
-				} else if (d >= 100)
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((d / 100) + '0');
-					_putchar(((d / 10) % 10) + '0');
-					_putchar((d % 10) + '0');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
