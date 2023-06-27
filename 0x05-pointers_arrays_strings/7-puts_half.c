e4e60cd9f726d6d4a0d5c674b66e1f8a13bb8a0d
setalxswe <setalxswe@gmail.com>
2023-06-26 21:31:00 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *puts_half - prints the second half of the string
+ *@str: take a string
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	i++;
+	for (i = i / 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
