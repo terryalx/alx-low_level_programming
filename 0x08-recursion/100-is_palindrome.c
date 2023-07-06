99c6fa9d874ecfd678336b96cd25fb129f43ed65
setalxswe <setalxswe@gmail.com>
2023-07-05 23:51:01 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * length -> returns the length of a string
+ * @s: string
+ * Return: length of string
+ */
+
+int length(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + length(s + 1));
+	}
+
+	return (0);
+}
+
+/**
+ * checker - helper function
+ * @s: string
+ * @x: num
+ * Return: output
+ */
+
+int checker(char *s, int x)
+{
+	if (x <= 0)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + x - 1))
+	{
+		return (checker(s + 1, x - 2));
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * is_palindrome -> return 1 if a string is a palindrome and 0 if not.
+ * @s: string
+ * Return: 1 if a string is a palindrome and 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	int x = length(s);
+
+	return (checker(s, x));
+}
