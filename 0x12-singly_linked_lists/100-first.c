6eaeefba7a51879e9d2685c5d8f6442d66242421
setalxswe <setalxswe@gmail.com>
2023-07-27 18:31:22 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+  *print - print before main
+  *
+  *Return: void
+  */
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
