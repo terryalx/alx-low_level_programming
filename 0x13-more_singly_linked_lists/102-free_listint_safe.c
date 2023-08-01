d2fbf9991a5d632320cc197ae514c8cded58adbb
setalxswe <setalxswe@gmail.com>
2023-08-01 11:31:50 -0700

    free a listint_t list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - free a list in safe mode
+ * @h: *head
+ * Description: this function free a string in a safe mode
+ * Return: the size of the list
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmp;
+	listint_t *actual;
+	size_t i;
+	int rest;
+
+	i = 0, actual = *h;
+
+	while (actual)
+	{
+		rest = actual - actual->next;
+		if (rest > 0)
+		{
+			tmp = actual->next;
+			free(actual);
+			actual = tmp;
+			i++;
+		} else
+		{
+			free(actual);
+			*h = NULL;
+			i++;
+			break;
+		}
+
+	}
+
+	*h = NULL;
+
+	return (i);
+}
