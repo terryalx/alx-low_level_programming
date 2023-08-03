6d22cd79fe6b55f85a24d1e64b1021dce921550d
setalxswe <setalxswe@gmail.com>
2023-08-03 04:47:04 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - get the value of a bit at a given index[]
+ * @n: bit int
+ * @index: index value at 0
+ * Return: If error -1 or value -> bit at index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int h;
+
+	if (index >= sizeof(n) * h)
+		return (-1);
+
+	return ((n >> index) & 1);
+}
