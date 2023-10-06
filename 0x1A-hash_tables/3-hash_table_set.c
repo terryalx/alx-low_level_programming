e8bd536116d8d96f6db961bc8984a231290852a7
setalxswe <setalxswe@gmail.com>
2023-10-05 17:29:39 -0700

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * replace_value - replaces the value at a pre-existing key
- * @ht: double pointer to the hash_node_t list
- * @key: new key to add in the node
- * @value: value to add in the node
+ * replace_value - Replaces the value associated with a pre-existing key.
+ * @ht: Double pointer to the hash_node_t list.
+ * @key: The new key to associate with the value.
+ * @value: The value to store in the node.
  */
 void replace_value(hash_node_t **ht, const char *key, const char *value)
 {
@@ -13,16 +13,19 @@ void replace_value(hash_node_t **ht, const char *key, const char *value)
 	while (temp && strcmp(temp->key, key))
 		temp = temp->next;
 
-	free(temp->value);
-	temp->value = strdup(value);
+	if (temp)
+	{
+		free(temp->value);
+		temp->value = strdup(value);
+	}
 }
 
 /**
- * check_key - checks if a key exists in a hash table
- * @ht: pointer to the hash_node_t list
- * @key: key to look for
+ * check_key - Checks if a key exists in a hash table.
+ * @ht: Pointer to the hash_node_t list.
+ * @key: The key to look for.
  *
- * Return: 1 if the key is found, 0 otherwise
+ * Return: 1 if the key is found, 0 otherwise.
  */
 int check_key(hash_node_t *ht, const char *key)
 {
@@ -32,17 +35,16 @@ int check_key(hash_node_t *ht, const char *key)
 			return (1);
 		ht = ht->next;
 	}
-
 	return (0);
 }
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the hash_node_t list
- * @key: new key to add in the node
- * @value: value to add in the node
+ * add_node - Adds a new node at the beginning of a linked list.
+ * @head: Double pointer to the hash_node_t list.
+ * @key: The new key to add to the node.
+ * @value: The value to add to the node.
  *
- * Return: the address of the new element, or NULL if it fails
+ * Return: The address of the new element, or NULL if it fails.
  */
 hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
 {
@@ -57,24 +59,24 @@ hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
 
 	if (*head == NULL)
 	{
-		(*head) = new;
 		new->next = NULL;
-	} else
-	{
-		new->next = (*head);
-		(*head) = new;
 	}
+	else
+	{
+		new->next = *head;
+	}
+	*head = new;
 
 	return (*head);
 }
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: hash table to add the element to
- * @key: key of the element, will give the index in the array
- * @value: value of the element to store in the array
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table to add the element to.
+ * @key: The key of the element, which determines the index in the array.
+ * @value: The value of the element to store in the array.
  *
- * Return: 1 on success, 0 otherwise
+ * Return: 1 on success, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -88,10 +90,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (check_key(ht->array[index], key))
 	{
 		replace_value(&ht->array[index], key, value);
-		return (1);
 	}
-	add_node(&ht->array[index], key, value);
-	if (&ht->array[index] == NULL)
-		return (0);
+	else
+	{
+		if (!add_node(&ht->array[index], key, value))
+			return (0);
+	}
+
 	return (1);
 }
+
