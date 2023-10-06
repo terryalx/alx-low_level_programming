dd8fc7307f0ffd933363dd0408aaba74f7f288ee
setalxswe <setalxswe@gmail.com>
2023-10-05 17:48:12 -0700

    __5-hash__

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the hash_node_t list to print
+ * print_list - Prints all the elements of a linked list.
+ * @h: Pointer to the hash_node_t list to print.
  */
 void print_list(hash_node_t *h)
 {
@@ -10,14 +10,16 @@ void print_list(hash_node_t *h)
 	{
 		printf("'%s': '%s'", h->key, h->value);
 		if (h->next)
+		{
 			printf(", ");
+		}
 		h = h->next;
 	}
 }
 
 /**
- * hash_table_print - prints a hash table
- * @ht: hash table to print
+ * hash_table_print - Prints a hash table.
+ * @ht: Hash table to print.
  */
 void hash_table_print(const hash_table_t *ht)
 {
@@ -27,12 +29,16 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int index;
 
 	if (!ht)
+	{
 		return;
+	}
 
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
+		{
 			node = ht->array[i];
+		}
 	}
 
 	printf("{");
@@ -45,9 +51,12 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			print_list(ht->array[i]);
 			if (ht->array[i] && i < index)
+			{
 				printf(", ");
+			}
 		}
 	}
 
 	printf("}\n");
 }
+
