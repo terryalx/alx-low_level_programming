e2d75e18382cbfac3359da271e1f07cee4b7501b
setalxswe <setalxswe@gmail.com>
2023-10-05 17:25:04 -0700

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -11,12 +11,9 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash;
-	val = 0;
 
-	if (size == val)
-	{
-		return (val);
-	}
+	if (size == 0)
+		return (0);
 
 	hash = hash_djb2(key);
 
