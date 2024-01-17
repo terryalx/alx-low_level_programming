15fcbb8437db4f444aba752016483a808e467fc6
setalxswe <setalxswe@gmail.com>
2024-01-17 00:51:37 -0800

    binary serach

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,45 +1,61 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in a sorted array
- * @array: pointer to the first element of the array
- * @size: number of elements in the array
- * @value: value to search for
- * Return: -1 if not found || index of value
+ * recursive_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int recursive_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+		return ((int)half);
+
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
 int binary_search(int *array, size_t size, int value)
 {
-	/* Check if the array is NULL */
-	if (array == NULL)
+	int index;
+
+	index = recursive_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
 		return (-1);
 
-	/* Initialize variables for binary search */
-	size_t i, right, left;
-
-	/* Binary search loop */
-	for (left = 0, right = size - 1; right >= left;)
-	{
-		/* Print the current sub-array being searched */
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
-
-		/* Calculate the middle index of the current sub-array */
-		i = left + (right - left) / 2;
-
-		/* Check if the middle element is the target value */
-		if (array[i] == value)
-			return (i);
-
-		/* If the middle element is greater, search the left sub-array */
-		if (array[i] > value)
-			right = i - 1;
-		/* If the middle element is smaller, search the right sub-array */
-		else
-			left = i + 1;
-	}
-
-	/* If the value is not found, return -1 */
-	return (-1);
+	return (index);
 }
