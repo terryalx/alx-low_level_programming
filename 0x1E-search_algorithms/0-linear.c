5023929acf5d3ffdaefb600334a69fe517ebe142
setalxswe <setalxswe@gmail.com>
2024-01-17 00:04:10 -0800

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,9 +2,13 @@
 
 /**
  * linear_search - searches for a value in an array
+ *
  * @array: pointer to the first element of the array
+ *
  * @size: number of elements in an array
+ *
  * @value: value to search for
+ *
  * Return: index of value || -1 if not found
  */
 
@@ -12,7 +16,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	/* Check if the input array is NULL */
+	/* Checks if array is NULL */
 	if (array == NULL)
 		return (-1);
 
@@ -26,6 +30,7 @@ int linear_search(int *array, size_t size, int value)
 		if (array[i] == value)
 			return (i);
 	}
-	/* Value not found in the array */
+
+	/* Value not found in the array : exit -1 */
 	return (-1);
 }
